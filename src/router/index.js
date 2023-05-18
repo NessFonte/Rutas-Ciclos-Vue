42f3ed3854179bff1ea95c165a2a67a391b6ec86
neisser <neisser2909@gmail.com>
2023-05-18 11:22:36 -0400

    Creado el componente NotFound para URLs erroneas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import PokemonAbout from '@/modules/pokemon/pages/PokemonAbout.vue'
 import PokemonList from '@/modules/pokemon/pages/PokemonList.vue'
 import Pokemon from '@/modules/pokemon/pages/Pokemon.vue'
+import NotFound from '@/modules/shared/pages/NotFound.vue'
 
 import {createRouter, createWebHashHistory} from 'vue-router'
 
@@ -8,6 +9,7 @@ const routes = [
     { path: '/', component: PokemonList },
     { path: '/about', component: PokemonAbout },
     { path: '/id', component: Pokemon },
+    { path: '/:pathMatch(.*)*', component: NotFound },
 ]
 
 const router = createRouter({
