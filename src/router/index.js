93815a3c50383a9157944161799504f8e5dcb924
neisser <neisser2909@gmail.com>
2023-05-18 11:49:02 -0400

    Creada la navegación con el LazyLoad para minimizar la carga de los componentes al llamarlos

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import PokemonAbout from '@/modules/pokemon/pages/PokemonAbout.vue'
+/*import PokemonAbout from '@/modules/pokemon/pages/PokemonAbout.vue'
 import PokemonList from '@/modules/pokemon/pages/PokemonList.vue'
 import Pokemon from '@/modules/pokemon/pages/Pokemon.vue'
-import NotFound from '@/modules/shared/pages/NotFound.vue'
+import NotFound from '@/modules/shared/pages/NotFound.vue'*/
 
 import {createRouter, createWebHashHistory} from 'vue-router'
 
 const routes = [
-    { path: '/', component: PokemonList },
-    { path: '/about', component: PokemonAbout },
-    { path: '/id', component: Pokemon },
-    { path: '/:pathMatch(.*)*', component: NotFound },
+    { path: '/', component: () => import(/*webpackChunkName: "PokemonList"*/ '@/modules/pokemon/pages/PokemonList.vue') },
+    { path: '/about', component: () => import(/*webpackChunkName: "PokemonAbout"*/ '@/modules/pokemon/pages/PokemonAbout.vue') },
+    { path: '/id', component: () => import(/*webpackChunkName: "Pokemon"*/ '@/modules/pokemon/pages/Pokemon.vue') },
+    { path: '/:pathMatch(.*)*', component: () => import(/*webpackChunkName: "NotFound"*/ '@/modules/shared/pages/NotFound.vue') },
 ]
 
 const router = createRouter({
