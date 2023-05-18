f172d51bd6df960600cbfd7e8334962a3c92fe9a
neisser <neisser2909@gmail.com>
2023-05-18 12:03:15 -0400

    Creado el Navbar para navegación entre las páginas

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <router-link to="/">Pokemon list</router-link>
+        <router-link to="/id">Pokemon for id</router-link>
+        <router-link to="/about">Pokemon about</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+div {
+    padding: 0 30px;
+}
+
+div a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 0 10px;
+}
+
+</style>
