12b9b9e2f43d7b6d1acecce447a856ebc5696273
neisser <neisser2909@gmail.com>
2023-05-18 13:21:42 -0400

    Creada prop id para recibirla como parámetro

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,13 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 const routes = [
     { path: '/', component: () => import(/*webpackChunkName: "PokemonList"*/ '@/modules/pokemon/pages/PokemonList.vue') },
     { path: '/about', component: () => import(/*webpackChunkName: "PokemonAbout"*/ '@/modules/pokemon/pages/PokemonAbout.vue') },
-    { path: '/id', component: () => import(/*webpackChunkName: "Pokemon"*/ '@/modules/pokemon/pages/Pokemon.vue') },
+    { path: '/:id', name: 'pokemon-id',component: () => import(/*webpackChunkName: "Pokemon"*/ '@/modules/pokemon/pages/Pokemon.vue'),
+        props: (route) => {
+            console.log(route)
+            const id = Number(route.params.id)
+            return isNaN(id) ? {id: 1} : {id}
+        }
+    },
     { path: '/:pathMatch(.*)*', component: () => import(/*webpackChunkName: "NotFound"*/ '@/modules/shared/pages/NotFound.vue') },
 ]
 
