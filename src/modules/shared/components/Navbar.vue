a876aeaa82b7075d8ffb54742dfd83e13f50a9e5
neisser <neisser2909@gmail.com>
2023-05-18 20:09:48 -0400

    Agregados los links para navegar mediante el Layout de Dragon Ball

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
+        <!--Poquemons links-->
         <router-link :to="{name: 'pokemon-home'}">Pokemon list</router-link>
         <router-link :to="{name: 'pokemon-id', params: {id: 1}}">Pokemon for id</router-link>
         <router-link :to="{name: 'pokemon-about'}">Pokemon about</router-link>
+
+        <!--Dragon Ball Z links-->
+        <router-link :to="{name: 'dbz-home'}">DBZ Characters</router-link>
+        <router-link :to="{name: 'dbz-about'}">About DBZ</router-link>
     </div>
 </template>
 
